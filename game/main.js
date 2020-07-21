985c94ed6537e3a842146bd9a9faf92a622c22da
Asher Ferri <asher@Ashers-MacBook-Pro.local>
2020-07-21 11:56:17 -0400

    pixel moves with key's input over lifeless grid

--- game/main.js
+++ game/main.js
@@ -1,10 +1,37 @@
-const toggleElement = document.getElementById('container')
-    console.log(toggleElement)
+// const toggleElement = document.getElementById('container')
+//     console.log(toggleElement)
 
-    for (i = 0; i <= 10086; i++){
-        const newCell = document.createElement('div');
-            newPixels.classList.add('pixel')
-            greyScreen.appendChild(newPixels)
-            //parent                  (child)
-            newPixels.addEventListener('mouseenter', colorPixel)
+//     for (i = 0; i <= 10086; i++){
+//         const newCell = document.createElement('div');
+//             newPixels.classList.add('pixel')
+//             greyScreen.appendChild(newPixels)
+//             //parent                  (child)
+//             newPixels.addEventListener('mouseenter', colorPixel)
+//     }
+
+let mazeRunner = document.querySelector('#mazeRunner')
+
+let moveDistance = 20
+
+window.addEventListener('load', () => {
+    mazeRunner.style.position = 'relative'
+        mazeRunner.style.left = 0
+        mazeRunner.style.top = 0
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'ArrowLeft':
+            mazeRunner.style.left = parseInt(mazeRunner.style.left) - moveDistance + 'px'
+                break
+        case 'ArrowRight':
+            mazeRunner.style.left = parseInt(mazeRunner.style.left) + moveDistance + 'px'
+                break
+        case 'ArrowUp':
+            mazeRunner.style.top = parseInt(mazeRunner.style.top) - moveDistance + 'px'
+                break
+        case 'ArrowDown':
+            mazeRunner.style.top = parseInt(mazeRunner.style.top) + moveDistance + 'px'
+                break
     }
+})

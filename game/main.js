06bbf2a6e9829327292eda7c9a58fad40d86eb87
Asher Ferri <37329444+asherferri@users.noreply.github.com>
2020-07-21 10:46:04 -0400

    added file structure

---
+++ game/main.js
@@ -0,0 +1,10 @@
+const toggleElement = document.getElementById('container')
+    console.log(toggleElement)
+
+    for (i = 0; i <= 10086; i++){
+        const newCell = document.createElement('div');
+            newPixels.classList.add('pixel')
+            greyScreen.appendChild(newPixels)
+            //parent                  (child)
+            newPixels.addEventListener('mouseenter', colorPixel)
+    }
